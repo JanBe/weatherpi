787af681b7d6dd836e5833b137b2bd7c946b340c
Jan Bechstein <jan@bechstein.me>
2016-12-18 02:52:49 +0100

    Handle connection problems with weather API

--- weatherclock.py
+++ weatherclock.py
@@ -39,16 +39,26 @@ def display_time_for_sixty_seconds():
 
 # Scrolls the sunrise, sunset and current forecast once
 def display_astronomy_and_forecast(weather_forecast, astronomy):
-    print(sunrise_and_sunset_text(astronomy) +' '+ forecast_text_for_today(weather_forecast))
+    if(weather_forecast != {} and astronomy != {}):
+        print(sunrise_and_sunset_text(astronomy) +' '+ forecast_text_for_today(weather_forecast))
 
 # Display the current time for 60 seconds, scroll the forecast, go back to the current time
 # Forecast is refreshed once per hour
 def run(api_key, country, city):
+    weather_forecast = get_wunderground_data(api_key, 'forecast', country, city)
+    astronomy = get_wunderground_data(api_key, 'astronomy', country, city)
     while True:
-        one_hour_from_now = time.time() + 60 * 60
-        weather_forecast = get_wunderground_data(api_key, 'forecast', country, city)
-        astronomy = get_wunderground_data(api_key, 'astronomy', country, city)
-        while time.time() < one_hour_from_now:
+        refresh_time = time.time() + 60 * 60
+        new_weather_forecast = get_wunderground_data(api_key, 'forecast', country, city)
+        new_astronomy = get_wunderground_data(api_key, 'astronomy', country, city)
+
+        if(new_weather_forecast == {} or new_astronomy == {}):
+            refresh_time = time.time() + 30
+        else:
+            weather_forecast = new_weather_forecast
+            astronomy = new_astronomy
+
+        while time.time() < refresh_time:
             display_time_for_sixty_seconds()
             display_astronomy_and_forecast(weather_forecast, astronomy)
 
