54684657841f5218a8a678da76faeae95142f465
Jan Bechstein <jan@bechstein.me>
2016-12-18 17:01:35 +0100

    Refactoring after testing on live hardware

--- weatherclock.py
+++ weatherclock.py
@@ -31,41 +31,50 @@ def sunrise_and_sunset_text(astronomy):
     sunset = astronomy['sun_phase']['sunset']
     return 'Sunrise: '+ sunrise['hour'] +':'+ sunrise['minute'] +', Sunset: '+ sunset['hour'] +':'+ sunset['minute'] +'.'
 
-# Displays the current time for 60 seconds
-def display_time_for_sixty_seconds():
-    sixty_seconds_from_now = time.time() + 60
-    while time.time() < sixty_seconds_from_now:
-        scrollphat.write_string(time.strftime("%H:%M"))
-        time.sleep(1)
-
-# Scrolls the sunrise, sunset and current forecast once
-def display_astronomy_and_forecast(weather_forecast, astronomy):
-    if(weather_forecast != {} and astronomy != {}):
-        scrollphat.write_string(sunrise_and_sunset_text(astronomy) +' '+ forecast_text_for_today(weather_forecast))
-        scrollphat.scroll()
+# Returns the current time in a mixed hexadecimal and decimal representation.
+# The hours are between 0 and 12. For hours > 9, hexadecimal representation is used.
+# Minutes are displayed in the decimal system. No symbol seperates hours and minutes.
+# Examples: 3:20 = 320, 14:20 = 220, 10:15 = A15, 11:40 = B40, 12:25 = C25
+# This is done because the scrollphat can only display 3 digits without scrolling.
+def current_time():
+    current_time = time.localtime()
+    hours = '%X' % (current_time.tm_hour % 12)
+    minutes = current_time.tm_min
+    return hours + minutes
 
 # Display the current time for 60 seconds, scroll the forecast, go back to the current time
 # Forecast is refreshed once per hour
 def run(api_key, country, city):
     scrollphat.set_brightness(2)
     scrollphat.set_rotate(True)
-    weather_forecast = get_wunderground_data(api_key, 'forecast', country, city)
-    astronomy = get_wunderground_data(api_key, 'astronomy', country, city)
+    weather_forecast = {}
+    astronomy = {}
+    forecast_refresh_time = time.time()
     while True:
         try:
-            refresh_time = time.time() + 60 * 60
-            new_weather_forecast = get_wunderground_data(api_key, 'forecast', country, city)
-            new_astronomy = get_wunderground_data(api_key, 'astronomy', country, city)
+            if forecast_refresh_time < time.time():
+                new_weather_forecast = get_wunderground_data(api_key, 'forecast', country, city)
+                new_astronomy = get_wunderground_data(api_key, 'astronomy', country, city)
 
-            if(new_weather_forecast == {} or new_astronomy == {}):
-                refresh_time = time.time() + 30
-            else:
-                weather_forecast = new_weather_forecast
-                astronomy = new_astronomy
+                if(new_weather_forecast == {} or new_astronomy == {}):
+                    forecast_refresh_time = time.time()
+                else:
+                    weather_forecast = new_weather_forecast
+                    astronomy = new_astronomy
+                    forecast_refresh_time = time.time() + 60 * 60
 
-            while time.time() < refresh_time:
-                display_time_for_sixty_seconds()
-                display_astronomy_and_forecast(weather_forecast, astronomy)
+            # Display current time for 60 seconds
+            scrollphat.write_string(current_time())
+            sleep(60)
+
+            # Scroll forecast once
+            if(weather_forecast != {} and astronomy != {}):
+                scrollphat.write_string(sunrise_and_sunset_text(astronomy) +' '+ forecast_text_for_today(weather_forecast))
+                length = scrollphat.buffer_len()
+
+                for i in range(length):
+                    scrollphat.scroll()
+                    time.sleep(0.1)
 
         except KeyboardInterrupt:
             scrollphat.clear()
